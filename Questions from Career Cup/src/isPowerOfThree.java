374c19951e5a163312c547f5003ee1caabbbbacb
eshirima <eshirima@kent.edu>
2015-10-06 01:04:29 -0400

    Powers Of Three Question
    Finished the implementation

---
+++ Questions from Career Cup/src/isPowerOfThree.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+
+/**
+ * @author espunkid
+ *
+ */
+public abstract class isPowerOfThree
+{
+	public static int isPowerOfThree(long number)
+	{
+		double answer = Math.log10(number) / Math.log10(3);
+		if ((answer - (int) (answer)) == 0.0)
+		{
+			return 1;
+		} else
+		{
+			return 0;
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println(isPowerOfThree(27));
+		System.out.println(isPowerOfThree(9));
+		System.out.println(isPowerOfThree(42));
+		System.out.println(isPowerOfThree(0));
+
+	}
+
+}
