70b0f80a2c93e0a2917811f0ffe2f98e59c31e53
eshirima <eshirima@kent.edu>
2015-10-06 02:06:40 -0400

    Number Creation
    Added some test cases but haven’t finished implementing it yet

--- Questions from Career Cup/Digits/src/NumberCreation.java
+++ Questions from Career Cup/Digits/src/NumberCreation.java
@@ -30,6 +30,28 @@ public final class NumberCreation
 			first /= 10;
 			second /= 10;
 		}
+		int j = secondList.size() - 1;
+		for (int i = 0; i < firstList.size(); ++i)
+		{
+			if (firstList.get(i) > secondList.get(j))
+			{
+				// the larger digit was found at the end of the ArrayList
+				if (i == (firstList.size() - 1))
+				{
+
+				}
+				// the larger digit was found at the start of the ArrayList
+				else if (i == 0)
+				{
+
+				}
+				// the larger digit was found at some random spot in the ArrayList
+				else
+				{
+
+				}
+			}
+		}
 		return result;
 	}
 
